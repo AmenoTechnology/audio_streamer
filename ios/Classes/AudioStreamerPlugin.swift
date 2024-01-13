2671a37ddc4688f8f749712190d7dc7475aa5cd0
QuangPB <quangpb@ameno.tech>
2024-01-13 23:04:49 +0700

    revert

--- ios/Classes/AudioStreamerPlugin.swift
+++ ios/Classes/AudioStreamerPlugin.swift
@@ -73,24 +73,22 @@ public class AudioStreamerPlugin: NSObject, FlutterPlugin, FlutterStreamHandler 
       onBus: 0, bufferSize: 8196, format: inputFormat
     ) {
       (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
-      DispatchQueue.main.async { [weak self] in
-        guard let self = self, let eventSink = self.eventSink else {
-          return
-        }
-
-        let outputFrameCapacity = AVAudioFrameCount(Double(buffer.frameCapacity) * sampleRateRatio)
-
-        let newBuffer = AVAudioPCMBuffer(
-          pcmFormat: format16K, frameCapacity: outputFrameCapacity)!
-
-        let inputBlock: AVAudioConverterInputBlock = { inNumPackets, outStatus in
-          outStatus.pointee = .haveData
-          return buffer
-        }
-
-        converter.convert(to: newBuffer, error: nil, withInputFrom: inputBlock)
-        eventSink(newBuffer.data())
+      guard let eventSink = self.eventSink else {
+        return
       }
+
+      let outputFrameCapacity = AVAudioFrameCount(Double(buffer.frameCapacity) * sampleRateRatio)
+
+      let newBuffer = AVAudioPCMBuffer(
+        pcmFormat: format16K, frameCapacity: outputFrameCapacity)!
+
+      let inputBlock: AVAudioConverterInputBlock = { inNumPackets, outStatus in
+        outStatus.pointee = .haveData
+        return buffer
+      }
+
+      converter.convert(to: newBuffer, error: nil, withInputFrom: inputBlock)
+      eventSink(newBuffer.data())
     }
 
     audioEngine.prepare()
